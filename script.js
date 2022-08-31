8c8094d503150fd9b1b2fff376f2b4cfe7b4b6fb
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-08-31 15:20:38 -0300

    Adiciona espaçamento

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const palette = document.getElementById('color-palette');
+
 function addColor(color) {
   const newColor = document.createElement('div');
   newColor.className = 'color';
   newColor.style.backgroundColor = color;
   palette.appendChild(newColor);
 }
+
 addColor('black');
 addColor('red');
 addColor('green');
